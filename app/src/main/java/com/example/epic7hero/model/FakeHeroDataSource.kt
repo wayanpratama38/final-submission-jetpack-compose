64a1f560615d2829b7f0516e071a98bd0dfcdf85
wynprtm <wayanpratama38@gmail.com>
2025-01-31 04:52:09 +0700

    Creating Abigail Data

--- app/src/main/java/com/example/epic7hero/model/FakeHeroDataSource.kt
+++ app/src/main/java/com/example/epic7hero/model/FakeHeroDataSource.kt
@@ -1,5 +1,8 @@
 package com.example.epic7hero.model
 
+import androidx.compose.ui.res.integerResource
+import com.example.epic7hero.R
+
 object FakeHeroDataSource {
     val heroData = listOf(
         Hero(
@@ -7,7 +10,21 @@ object FakeHeroDataSource {
             name = "Abigail",
             heroLore = "Before she left Erasia, Melissa Bloodrose gave her beloved little sister Abigail the vampiric power of extended youth. After the departure of Abigail's favorite sister came her family's fall to ruin. As Abigail wandered, lost amidst life's hardships, she one day crossed paths with Vampire Lord Kane an gained the complete power of a vampire.",
             profileImage = "https://raw.githubusercontent.com/wayanpratama38/e7-heroes/main/asset/HeroImages/abigail/profile/abigail.png",
-            portraitImage = ""
+            portraitImage = "https://raw.githubusercontent.com/wayanpratama38/e7-heroes/main/asset/HeroImages/abigail/portrait/abigail.png",
+            element = "https://raw.githubusercontent.com/wayanpratama38/e7-heroes/main/asset/ElementImages/Fire-circle.png",
+            rarity = "https://raw.githubusercontent.com/wayanpratama38/e7-heroes/main/asset/HeroeRarityImages/5star.png",
+            classes = "https://raw.githubusercontent.com/wayanpratama38/e7-heroes/main/asset/JobImages/warrior.png",
+            zodiac = "https://raw.githubusercontent.com/wayanpratama38/e7-heroes/main/asset/ZodiacImages/aries.png",
+            heroStats = Stats(83,322,73,113),
+            skills = listOf(
+                Skill(
+                    skillName = "Ambush",
+                    skillImage = "https://raw.githubusercontent.com/wayanpratama38/e7-heroes/main/asset/HeroImages/abigail/skill/1_ambush.png",
+                    skillDescription = "Attacks the enemy with bloodied wings, and increases Combat Readiness of the caster by 15%. When used on the caster's turn, has a 35% chance to activate Might as an extra effect. Damage dealt increases proportional to the caster's max Health. Might: Increases Combat Readiness of all allies by 15%.",
+                    skillCooldown = 0,
+                    soulObtain = 1,
+                )
+            )
         )
     )
 }
--- app/src/main/java/com/example/epic7hero/model/Skill.kt
+++ app/src/main/java/com/example/epic7hero/model/Skill.kt
@@ -2,7 +2,7 @@ package com.example.epic7hero.model
 
 data class Skill(
     val skillName : String,
-    val skillImage : Int,
+    val skillImage : String,
     val skillDescription : String,
     val skillCooldown : Int = 0,
     val soulObtain : Int = 0
