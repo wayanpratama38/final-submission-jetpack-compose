14880af7953e11b90ebd74b52c983db09ac9beb1
wynprtm <wayanpratama38@gmail.com>
2025-02-01 05:59:46 +0700

    Modified Search Bar if Empty

--- app/src/main/java/com/example/epic7hero/ui/screen/home/Home.kt
+++ app/src/main/java/com/example/epic7hero/ui/screen/home/Home.kt
@@ -19,6 +19,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -93,12 +94,13 @@ fun HomeContent(
             )
         }else{
             Box(
-                modifier = modifier,
+                modifier = modifier.fillMaxSize(),
                 contentAlignment = Alignment.Center
             ){
                 Text(
                     text = stringResource(R.string.hero_not_found),
-                    fontSize = 24.sp
+                    fontSize = 24.sp,
+                    fontWeight = FontWeight.Bold
                 )
             }
 
